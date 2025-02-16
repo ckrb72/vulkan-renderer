169fd0050495695757680c304be9e36fc26ced89
ckrb72 <krck70153@gmail.com>
2025-02-15 23:42:13 -0500

    Rendering triangle now

--- src/main.cpp
+++ src/main.cpp
@@ -45,8 +45,10 @@ int main()
         framebuffers[i].color_buffers.push_back(context.swapchain.image_views[i]);
     }*/
 
+    queue_families queues = vk_get_device_queues(context.physical_device, context);
+
     vk_command_pool command_pool{};
-    vk_command_pool_create(context, &command_pool, vk_get_device_queues(context.physical_device, context).graphics);
+    vk_command_pool_create(context, &command_pool, queues.graphics);
 
     vk_command_pool_add_buffers(context, command_pool, MAX_FRAMES_IN_FLIGHT);
 
@@ -68,13 +70,29 @@ int main()
         }
     }
 
+    VkQueue graphics_queue;
+    VkQueue present_queue;
+
+    vkGetDeviceQueue(context.logical_device, queues.graphics, 0, &graphics_queue);
+    vkGetDeviceQueue(context.logical_device, queues.present, 0, &present_queue);
+
     uint32_t current_frame = 0;
+
+
+    PFN_vkCmdBeginRenderingKHR vkCmdBeginRenderingKHR_ext = (PFN_vkCmdBeginRenderingKHR)vkGetInstanceProcAddr(context.instance, "vkCmdBeginRenderingKHR");
+    PFN_vkCmdEndRenderingKHR vkCmdEndRenderingKHR_ext = (PFN_vkCmdEndRenderingKHR)vkGetInstanceProcAddr(context.instance, "vkCmdEndRenderingKHR");
+
+    if(!vkCmdBeginRenderingKHR_ext || !vkCmdEndRenderingKHR_ext)
+    {
+        std::cerr << "Failed to load rendering procs" << std::endl;
+        return -1;
+    }
 
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
-        /*vkWaitForFences(context.logical_device, 1, &in_flight[current_frame], VK_TRUE, UINT64_MAX);
+        vkWaitForFences(context.logical_device, 1, &in_flight[current_frame], VK_TRUE, UINT64_MAX);
 
         uint32_t image_index;
         VkResult acquire_result = vkAcquireNextImageKHR(context.logical_device, context.swapchain.swapchain, UINT64_MAX, image_available[current_frame], VK_NULL_HANDLE, &image_index);
@@ -104,8 +122,72 @@ int main()
         rendering_info.renderArea = VkRect2D{VkOffset2D{}, context.swapchain.extent};
         rendering_info.layerCount = 1;
         rendering_info.colorAttachmentCount = 1;
-        rendering_info.pColorAttachments = &color_attachment;*/
+        rendering_info.pColorAttachments = &color_attachment;
 
+        vkCmdBeginRenderingKHR_ext(command_pool.buffers[current_frame], &rendering_info);
+
+        // Rendering commands here
+        vkCmdBindPipeline(command_pool.buffers[current_frame], VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.pipeline);
+
+        VkViewport viewport{};
+        viewport.x = 0.0f;
+        viewport.y = 0.0f;
+        viewport.width = static_cast<float>(context.swapchain.extent.width);
+        viewport.height = static_cast<float>(context.swapchain.extent.height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+        vkCmdSetViewport(command_pool.buffers[current_frame], 0, 1, &viewport);
+
+        VkRect2D scissor{};
+        scissor.offset = {0, 0};
+        scissor.extent = context.swapchain.extent;
+        vkCmdSetScissor(command_pool.buffers[current_frame], 0, 1, &scissor);
+
+        vkCmdDraw(command_pool.buffers[current_frame], 3, 1, 0, 0);
+
+        vkCmdEndRenderingKHR_ext(command_pool.buffers[current_frame]);
+
+        if(vkEndCommandBuffer(command_pool.buffers[current_frame]) != VK_SUCCESS)
+        {
+            std::cerr << "Failed to end command buffer" << std::endl;
+            return -1;
+        }
+
+        VkSubmitInfo submit_info{};
+        submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        
+        VkSemaphore wait_semaphores[] = { image_available[current_frame] };
+        VkPipelineStageFlags wait_stages[] = { VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT };
+        submit_info.waitSemaphoreCount = 1;
+        submit_info.pWaitSemaphores = wait_semaphores;
+        submit_info.pWaitDstStageMask = wait_stages;
+        submit_info.commandBufferCount = 1;
+        submit_info.pCommandBuffers = &command_pool.buffers[current_frame];
+
+        VkSemaphore signal_semaphores[] = { render_finished[current_frame] };
+        submit_info.signalSemaphoreCount = 1;
+        submit_info.pSignalSemaphores = signal_semaphores;
+
+        if(vkQueueSubmit(graphics_queue, 1, &submit_info, in_flight[current_frame]) != VK_SUCCESS)
+        {
+            std::cerr << "Failed to submit to graphics queue" << std::endl;
+            return -1;
+        }
+
+        VkPresentInfoKHR present_info{};
+        present_info.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+        present_info.waitSemaphoreCount = 1;
+        present_info.pWaitSemaphores = signal_semaphores;
+
+        VkSwapchainKHR swapchains[] = { context.swapchain.swapchain };
+        present_info.swapchainCount = 1;
+        present_info.pSwapchains = swapchains;
+        present_info.pImageIndices = &image_index;
+        present_info.pResults = NULL;
+
+        VkResult present_result = vkQueuePresentKHR(present_queue, &present_info);
+
+        current_frame = (current_frame + 1) % MAX_FRAMES_IN_FLIGHT;
 
     }
 
